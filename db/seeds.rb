5abfdbfdeb9d0ff8df9f1416137d4ba92eca84fa
Dominic Brown <yeahdom@gmail.com>
2017-07-14 12:21:24 -0700

    add image_url to product model, show product's image when showing product details along with the destription

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@ class Seed
     20.times do |i|
       Product.create!(name: Faker::Lorem.word,
                       price: rand(10...100),
-                      description: Faker::Lorem.sentence(5, false, 0).chop
+                      description: Faker::Lorem.sentence(5, false, 0).chop,
+                      image_url: "https://tinyurl.com/yampnrh3"
                       )
     end
   end
